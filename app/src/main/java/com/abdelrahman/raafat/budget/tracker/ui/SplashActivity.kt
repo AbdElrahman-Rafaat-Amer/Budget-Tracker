39b61dcde3ac77812de29ec8a13c9540b8ecbfe9
Abdelrahman Raafat <abdelrahman.raafaat@gmail.com>
2024-11-16 00:58:35 +0200

    [BT-5]: https://abdelrahmanraafat.atlassian.net/browse/BT-5 - Created Splash Activity. - Created Splash Screen UI. - Added Lottie dependency on the app. - Created Onboarding Activity. - Navigate to the Onboarding after Lottie animation finished

---
+++ app/src/main/java/com/abdelrahman/raafat/budget/tracker/ui/SplashActivity.kt
@@ -0,0 +1,55 @@
+package com.abdelrahman.raafat.budget.tracker.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.tooling.preview.Preview
+import com.abdelrahman.raafat.budget.tracker.R
+import com.abdelrahman.raafat.budget.tracker.ui.onboarding.OnBoardingActivity
+import com.abdelrahman.raafat.budget.tracker.ui.theme.BudgetTrackerTheme
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BudgetTrackerTheme {
+
+                val preloaderLottieCompositionResult = rememberLottieComposition(
+                    LottieCompositionSpec.RawRes(
+                        R.raw.splash_icon
+                    )
+                )
+
+                val progress by animateLottieCompositionAsState(preloaderLottieCompositionResult.value)
+
+                LottieAnimation(
+                    composition = preloaderLottieCompositionResult.value,
+                )
+
+                if (progress == 1.0f) {
+                    startActivity(Intent(this, OnBoardingActivity::class.java))
+                    finish()
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    BudgetTrackerTheme {
+        SplashActivity()
+    }
+}
--- app/src/main/java/com/abdelrahman/raafat/budget/tracker/ui/theme/Theme.kt
+++ app/src/main/java/com/abdelrahman/raafat/budget/tracker/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.abdelrahman.raafat.budget.tracker.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,29 +8,61 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+// Light color scheme
+val LightColorScheme = lightColorScheme(
+    primary = GreenPrimary,
+    secondary = BluePrimary,
+    tertiary = OrangeAccent,
+    background = LightBackground,
+    surface = LightCardBackground,
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onSecondary = Color.Black,
+    onBackground = LightText,
+    onSurface = LightText
 )
+
+// Dark color scheme
+val DarkColorScheme = darkColorScheme(
+    primary = DarkPrimary,
+    secondary = BluePrimary,
+    tertiary = YellowAccent,
+    background = DarkBackground,
+    surface = DarkCardBackground,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = DarkText,
+    onSurface = DarkText
+)
+
+
+val OnboardingLightColorScheme = lightColorScheme(
+    primary = Teal500,
+    secondary = LightBlue,
+    tertiary = SoftGreen,
+    background = BackgroundLight,
+    surface = BackgroundLight,
+    onPrimary = Color.White,
+    onSecondary = TextPrimary,
+    onBackground = TextPrimary,
+    onSurface = TextPrimary
+)
+
+// Dark Color Scheme
+val OnboardingDarkColorScheme = darkColorScheme(
+    primary = Teal500,
+    secondary = LightBlue,
+    tertiary = MutedLavender,
+    background = BackgroundDark,
+    surface = BackgroundDark,
+    onPrimary = Color.White,
+    onSecondary = TextPrimary,
+    onBackground = Color.White,
+    onSurface = Color.White
+)
+
 
 @Composable
 fun BudgetTrackerTheme(
@@ -48,6 +79,31 @@ fun BudgetTrackerTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
+
+
+@Composable
+fun BudgetTrackerOnBoardingTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> OnboardingDarkColorScheme
+        else -> OnboardingLightColorScheme
     }
 
     MaterialTheme(
