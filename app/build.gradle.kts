7d53d42ab88deeb50b5a25f60a0ccfe6431f6626
Abdelrahman Raafat <abdelrahman.raafaat@gmail.com>
2024-11-15 20:37:27 +0200

    Update compileSdk. Add string Arabic file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.abdelrahman.raafat.budget.tracker"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.abdelrahman.raafat.budget.tracker"
