b03f4dc7d489ef9f538a5606fc052ed8711b0c7e
Abdelrahman Raafat <abdelrahman.raafaat@gmail.com>
2024-12-15 09:44:40 +0200

    [BT-15]: https://abdelrahmanraafat.atlassian.net/browse/BT-15 - Updated .gitignore

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring
 
 JNICALL
 Java_com_abdelrahman_raafat_budget_tracker_MainActivity_stringFromJNI(JNIEnv *env, jobject object) {
-    std::string hello = "Hello from Jni";
+    std::string hello = "Hello from dweweeJni";
     return env->NewStringUTF(hello.c_str());
 }
